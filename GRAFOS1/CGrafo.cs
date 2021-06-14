c8639a0a25fdeb71cf5257739fc40e36d0fc575a
Rodolfo <Rodolfo@DESKTOP-GCSQM94>
2021-06-14 19:31:03 -0400

    Grafos etapa 1 terminada

---
+++ GRAFOS1/CGrafo.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRAFOS1
+{
+    class CGrafo
+    {
+        private int[,] mAdyacencia;
+        private int[] indegree;
+        private int nodos;
+
+        public CGrafo(int pNodos)
+        {
+            nodos = pNodos;
+
+            //Intanciamos matriz de adyacencia
+            mAdyacencia = new int[nodos, nodos];
+
+            //Intanciamos areglo de indegree
+
+            indegree = new int[nodos];
+        }
+        public void AdicionarArista(int pNodoInicio, int pNodoFinal)
+        {
+            mAdyacencia[pNodoInicio, pNodoFinal] = 1;
+        }
+        //Nuevo
+        public void AdicionarArista(int pNodoInicio, int pNodoFinal, int pPeso)
+        {
+            mAdyacencia[pNodoInicio, pNodoFinal] = pPeso;
+        }
+        public void MuestraAdyacencia()
+        {
+            int n = 0;
+            int m = 0;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            for (n = 0; n < nodos; n++)
+                Console.Write("\t{0}", n);
+
+            Console.WriteLine();
+
+            for (n = 0; n < nodos; n++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.Write("\t{0}", mAdyacencia[n, m]);
+            }
+            Console.WriteLine();
+        }
+
+        public int ObtenAdyacencia(int pFila, int pColumna)
+        {
+            return mAdyacencia[pFila, pColumna];
+        }
+        public void CalcularIndegree()
+        {
+            int n = 0;
+            int m = 0;
+
+            for (n = 0; n < nodos; n++)
+            {
+                for (m = 0; n < nodos; m++)
+                {
+                    if (mAdyacencia[m, n] == 1)
+                        indegree[n]++;
+                }
+            }
+        }
+        public void MostrarIndegree()
+        {
+            int n = 0;
+            Console.ForegroundColor = ConsoleColor.White;
+            for (n = 0; n < nodos; n++)
+                Console.WriteLine("Nodo:{0},{1}", n, indegree[n]);
+        }
+        public int EncuentraI0()
+        {
+            bool encontrado = false;
+            int n = 0;
+
+            for (n = 0; n < nodos; n++)
+            {
+                if (indegree[n] == 0)
+                {
+                    encontrado = true;
+                    break;
+                }
+            }
+            if (encontrado)
+                return n;
+            else return -1;//Codigo que indica que no se ha encontrado 
+        }
+        public void DecrementaIndegree(int pNodo)
+        {
+            indegree[pNodo] = -1;
+
+            int n = 0;
+
+            for (n = 0; n < nodos; n++)
+            {
+                if (mAdyacencia[pNodo, n] == 1)
+                    indegree[n]--;
+            }
+        }
+    }
+}
+   
+       
--- GRAFOS1/Class1.cs
+++ GRAFOS1/Class1.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace GRAFOS1
 {
-    public class Class1
+     class Program
     {
+        static void Main(string[] args)
+        {
+
+            int inicio = 0;
+            int final = 0;
+            int distancia = 0;
+            int n = 0;
+            int cantNodos = 7;
+            string dato = "";
+            int actual = 0;
+            int columna = 0;
+
+
+            //Creamos el grafo
+            CGrafo miGrafo = new CGrafo(cantNodos);
+
+            miGrafo.AdicionarArista(0, 1, 2);
+            miGrafo.AdicionarArista(0, 3, 1);
+            miGrafo.AdicionarArista(1, 3, 3);
+            miGrafo.AdicionarArista(1, 4, 10);
+            miGrafo.AdicionarArista(2, 0, 4);
+            miGrafo.AdicionarArista(2, 5, 5);
+            miGrafo.AdicionarArista(3, 2, 2);
+            miGrafo.AdicionarArista(3, 4, 2);
+            miGrafo.AdicionarArista(3, 5, 8);
+            miGrafo.AdicionarArista(3, 6, 4);
+            miGrafo.AdicionarArista(4, 6, 6);
+            miGrafo.AdicionarArista(6, 5, 1);
+
+            miGrafo.MuestraAdyacencia();
+        }
     }
 }
